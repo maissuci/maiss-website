fb3e4678ae0df2a5145cc73fdfd3c7767d2f2cac
Aithy Ho <90644712+aithyho@users.noreply.github.com>
2026-04-28 12:41:26 -0700

    Aithy H -> Aithy

--- components/Board.tsx
+++ components/Board.tsx
@@ -131,7 +131,7 @@ const boardMembers = [
     },
     {
         id: 22, 
-        name: "Aithy H", 
+        name: "Aithy", 
         position: "Professional Development Intern", 
         image: "/images/people/aithy.JPG", 
     }, 
